0d66b31e6e8579fbecaa6e1aa4c7a61a9a52b804
katrinwab <katrinwab@gmail.com>
2016-04-14 13:43:10 +0300

    add tests

---
+++ UnitTestExample/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using examples;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestExample
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(6, Program.Area(5, 3, 4));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            try
+            {
+                Program.Area(10, 3, 4);
+                Assert.Fail("test2 - 10 3 4. An exception should have been thrown");
+            }
+            catch (WrongLengthException ex)
+            {
+                Assert.AreEqual("Неверная длина сторон", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Unexpected exception");
+            }
+        }
+    }
+}
